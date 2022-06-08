cc2e02dfd81073940c06f497147e03f893880ef6
vcentsu <vincenttius.sutanto@gmail.com>
2022-06-08 16:57:48 +0700

    Initial Commit

---
+++ CarouselMovieApp/CarouselMovieAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CarouselMovieAppApp.swift
+//  CarouselMovieApp
+//
+//  Created by Vincentius Sutanto on 08/06/22.
+//
+
+import SwiftUI
+
+@main
+struct CarouselMovieAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CarouselMovieApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CarouselMovieApp
+//
+//  Created by Vincentius Sutanto on 08/06/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
