24149cd4b6cc86d9e51b3b2c90903f47df74c8a5
vcentsu <vincenttius.sutanto@gmail.com>
2022-06-13 13:45:40 +0700

    added data to second version

--- CarouselMovieApp/View/Home.swift
+++ CarouselMovieApp/View/Home.swift
@@ -11,65 +11,82 @@ struct Home: View {
     
     //@GestureState var offset: CGFloat = 0
     @State var currentIndex: Int = 0
-    @State var posts: [Post] = []
-    @State var currentTab = "Recent"
-    @Namespace var animation
+    //@State var posts: [Post] = []
+    //@State var currentTab = "Recent"
+    //@Namespace var animation
     
     var body: some View {
         
         VStack(spacing: 15){
             
-            VStack(alignment: .leading, spacing: 12){
-                
-                Button {
-                    
-                } label: {
-                    Label {
-                        Text("Back")
-                            .fontWeight(.semibold)
-                    } icon: {
-                        Image(systemName: "chevron.left")
-                            .font(.title2.bold())
-                    }
-                    .foregroundColor(.primary)
-                }
-                
-                Text("Marvel Movies")
-                    .font(.title)
-                    .fontWeight(.black)
-            }
-            .frame(maxWidth: .infinity, alignment: .leading)
-            .padding()
-            
-            //Segment Control
-            HStack(spacing: 0){
-                TabButton(title: "Recent", animation: animation, currentTab: $currentTab)
-                TabButton(title: "Favorites", animation: animation, currentTab: $currentTab)
-            }
-            .background(Color.black.opacity(0.04), in: RoundedRectangle(cornerRadius: 10))
-            .padding(.horizontal)
+//            VStack(alignment: .leading, spacing: 12){
+//
+//                Button {
+//
+//                } label: {
+//                    Label {
+//                        Text("Back")
+//                            .fontWeight(.semibold)
+//                    } icon: {
+//                        Image(systemName: "chevron.left")
+//                            .font(.title2.bold())
+//                    }
+//                    .foregroundColor(.primary)
+//                }
+//
+//                Text("Marvel Movies")
+//                    .font(.title)
+//                    .fontWeight(.black)
+//            }
+//            .frame(maxWidth: .infinity, alignment: .leading)
+//            .padding()
             
             
             //Snap Carousel
-            SnapCarousel(index: $currentIndex, items: posts) {post in
+            SnapCarousel(trailingSpace: 150, index: $currentIndex, items: posts) {post in
                 
-                GeometryReader { proxy in
-                    let size = proxy.size
+                VStack(spacing: 10){
+                    GeometryReader { proxy in
+                        let size = proxy.size
+                        
+                        Image(post.postImage)
+                            .resizable()
+                            .aspectRatio(contentMode: .fill)
+                            .frame(width: size.width, height: size.height)
+                            .cornerRadius(25)
+                    }
+                    .frame(height: getRect().height / 2.5)
+                    .padding(.bottom, 15)
                     
-                    Image(post.postImage)
-                        .resizable()
-                        .aspectRatio(contentMode: .fill)
-                        .frame(width: size.width)
-                        .cornerRadius(12)
+                    //Data
+                    Text(post.title)
+                        .font(.title2.bold())
+                    
+                    HStack(spacing: 3){
+                        ForEach(1...5, id: \.self){ index in
+                            
+                            Image(systemName: "star.fill")
+                                .foregroundColor(index <= post.starRating ? .yellow : .gray)
+                        }
+                        
+                        Text("(\(post.starRating).0)")
+                    }
+                    .font(.caption)
+                    
+                    Text(post.desc)
+                        .font(.callout)
+                        .lineLimit(3)
+                        .multilineTextAlignment(.center)
+                        .padding(.top, 8)
+                        .padding(.horizontal)
                 }
             }
-            .padding(.vertical, 40)
             
             //Indicator (Custom Paging Control)
             HStack (spacing: 10) {
-                
+
                 ForEach ( posts.indices, id: \.self){ index in
-                    
+
                     Circle()
                         .fill(Color.black.opacity(currentIndex == index ? 1 : 0.1))
                         .frame(width: 10, height: 10)
@@ -80,16 +97,23 @@ struct Home: View {
             .padding(.bottom, 40)
         }
         .frame(maxHeight: .infinity, alignment: .top)
-        .onAppear{
-            for index in 1...7 {
-                posts.append(Post(postImage: "post\(index)"))
-            }
-        }
+//        .onAppear{
+//            for index in 1...7 {
+//                posts.append(Post(postImage: "post\(index)"))
+//            }
+//        }
     }
 }
 
 struct Home_Previews: PreviewProvider {
     static var previews: some View {
         Home()
+    }
+}
+
+//Screen Bounds
+extension View{
+    func getRect() -> CGRect{
+        return UIScreen.main.bounds
     }
 }
